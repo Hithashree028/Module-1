7d6493b21b00c1f346e961953944bb198575a31b
Hithashree028 <hithashree028@gmail.com>
2025-08-18 19:23:07 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class add{
+    public static void main(String []args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter Two numbers");
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = a+b;
+    System.out.println("Addition is "+c);
+    }
+
+}
