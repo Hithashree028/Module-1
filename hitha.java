c8da07bb1f28055e196a27ea04af314dd0720334
Hithashree H H <hithashree028@gmail.com>
2025-08-19 15:13:12 +0530

    Add files via upload

---
+++ hitha.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class hitha {
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int a=sc.nextInt();
+        if(a>0){
+            System.out.println("The number is positive");
+        }
+        else{
+            System.out.println("The number is negative");
+        }
+    }
+    
+}
